f2b4afb66ccebf899527eba627a27f2137fb278d
Zhong Zhenyu <nczzy1997@gmail.com>
2019-11-18 15:51:37 +0800

    调整项目结构

--- ascii_art/ascii_art.py
+++ ascii_art/ascii_art.py
@@ -2,50 +2,44 @@ import argparse
 
 from PIL import Image
 
-__all__ = ['grayscale_image', 'ascii_art', 'print_art']
-default_charset = '$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,"^`\'. '
+DEFAULT_CHARSET = '$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,"^`\'. '
 
 
 def get_args():
     parser = argparse.ArgumentParser(
         description='create beautiful ascii arts',
-        allow_abbrev=False
-    )
+        allow_abbrev=False)
     parser.add_argument(
         '-c',
         '--charset',
         type=str,
-        default=[default_charset],
+        default=[DEFAULT_CHARSET],
         nargs=1,
-        help='set the charset used in the output'
-    )
+        help='set the charset used in the output')
     parser.add_argument(
         '-r',
         '--ratio',
         type=float,
         default=[2.25],
         nargs=1,
-        help='set image aspect ratio'
-    )
+        help='set the aspect ratio of the image')
     parser.add_argument(
         '-m',
         '--max-size',
         type=int,
         default=[96],
         nargs=1,
-        help='set the max size of the output'
-    )
+        help='set the max size of the output')
     parser.add_argument(
         'infile',
         metavar='IMG',
         type=str,
         nargs=1,
-        help='original image'
-    )
+        help='original image')
     return parser.parse_args()
 
 
-def grayscale_image(infile, max_size, ratio):
+def to_grayscale(infile, max_size=96, ratio=2.25):
     img = Image.open(infile)
     if img.size[0] > max_size:
         t_width = max_size
@@ -56,21 +50,22 @@ def grayscale_image(infile, max_size, ratio):
     return img.resize((t_width, t_height), Image.LANCZOS).convert('L')
 
 
-def ascii_art(img, charset=default_charset):
+def make_art(img, charset=DEFAULT_CHARSET):
     pixels = list(img.getdata())
     step = 256 / len(charset)
     width, height = img.size
-    ascii_art = []
+    art = []
     for y in range(0, height):
         line = []
         for x in range(0, width):
             index = int(pixels[x + y * width] / step)
             line.append(charset[index])
-        ascii_art.append(line)
-    return ascii_art
+        art.append(line)
+    return art
 
-def print_art(ascii_art):
-    for line in ascii_art:
+
+def print_art(art):
+    for line in art:
         for char in line:
             print(char, end='')
         print()
@@ -78,5 +73,5 @@ def print_art(ascii_art):
 
 if __name__ == '__main__':
     args = get_args()
-    image = grayscale_image(args.infile[0], args.max_size[0], args.ratio[0])
-    print_art(ascii_art(image, args.charset[0]))
+    image = to_grayscale(args.infile[0], args.max_size[0], args.ratio[0])
+    print_art(make_art(image, args.charset[0]))
